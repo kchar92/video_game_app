c5f8334d5e85e069c858c797c42e5630beb17e9c
Keelana Char <keelanachar@gmail.com>
2019-11-23 18:11:50 -0600

    used faker to generate usernames, and videogame details

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  VideoGame.create!(
+    title: Faker::Game.title,
+    description: Faker::Game.genre,
+    price: Faker::Number.between(from: 10, to: 100),
+    rating: ["E", "T", "M"].sample
+    )
+end
