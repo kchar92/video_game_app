a9411b6087ec9c1018a77db1de13f9a3056211fd
Keelana Char <keelanachar@gmail.com>
2019-11-23 18:41:44 -0600

    create PlayerVideoGame join table connections

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-  VideoGame.create!(
-    title: Faker::Game.title,
-    description: Faker::Game.genre,
-    price: Faker::Number.between(from: 10, to: 100),
-    rating: ["E", "T", "M"].sample
-    )
-end
+# 10.times do
+#   VideoGame.create!(
+#     title: Faker::Game.title,
+#     description: Faker::Game.genre,
+#     price: Faker::Number.between(from: 10, to: 100),
+#     rating: ["E", "T", "M"].sample
+#     )
+# end
+
+# 14.times do
+#   PlayerVideoGame.create!(
+#     video_game_id: [2, 4, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19].sample,
+#     player_id: rand(1..14)
+#     )
+# end
